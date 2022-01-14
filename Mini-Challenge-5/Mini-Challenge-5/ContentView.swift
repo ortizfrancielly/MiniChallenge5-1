6c7efc83f460b1efdf37dd3bfcb92409b0c1bb4a
ortizfrancielly <ortizfrancielly@gmail.com>
2022-01-14 18:17:51 -0300

    First Commit

---
+++ Mini-Challenge-5/Mini-Challenge-5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mini-Challenge-5
+//
+//  Created by Francielly Cristina Ortiz Candido on 14/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mini-Challenge-5/Mini-Challenge-5/Mini_Challenge_5App.swift
@@ -0,0 +1,17 @@
+//
+//  Mini_Challenge_5App.swift
+//  Mini-Challenge-5
+//
+//  Created by Francielly Cristina Ortiz Candido on 14/01/22.
+//
+
+import SwiftUI
+
+@main
+struct Mini_Challenge_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
